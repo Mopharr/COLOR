6d5b6db333bf66aa49506fba2b5655e46bb405f9
Mopharr <olamiquadri1@gmail.com>
2020-12-28 01:23:29 +0100

    second commit

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
- 
+ const menu = document.querySelector('#mobile-menu')
+ const menuLinks = document.querySelector('.navbar-menu')
+
+ // Display mobile menu
+ const mobilemenu = () => {
+     menu.classList.toggle('is-active')
+     menuLinks.classList.toggle('active')
+ }
+
+ menu.addEventListener('click', mobilemenu)
